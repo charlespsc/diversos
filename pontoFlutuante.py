4f2a3fca6fbc8c54522e832ccf6930abe0122544
Charles Pereira <charlespereira.fln@gmail.com>
2023-07-03 11:52:15 -0300

    exemplos

---
+++ pontoFlutuante.py
@@ -0,0 +1,8 @@
+# Podemos representar números muito grandes usando a representação de ponto flutuante. 
+# Essa representação tem um grande ganho em eficiência, mas causa erros de arredondamento.
+# Mesmo assim, se você programa com a linguagem Python, verá que você pode representar números gigantescos,
+# como dois elevado a mil, sem erros de arredondamento. 
+
+p = 2 ** 1000
+
+print(p)
